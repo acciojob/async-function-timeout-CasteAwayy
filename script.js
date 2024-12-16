e841261d0cc172936fed74ca72fe8a946d60714c
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-16 21:28:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const form = document.querySelector("form");
+const output = document.querySelector("#output");
+
+form.addEventListener("submit", handleSubmit);
+
+const promise = function (time) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve("ok");
+    }, time);
+  });
+};
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const waitOver = await promise(formData.get("delay"));
+  let html = `<p>${formData.get("text")}</p>`;
+  output.insertAdjacentHTML("afterbegin", html);
+}
